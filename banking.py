b7c1510993b30a7d282db81b85324ef910d7fde7
jeenakarmi <jinanakarmi@gmail.com>
2024-03-23 16:35:09 +0545

    created n objects and put in list

--- banking.py
+++ banking.py
@@ -1,23 +1,35 @@
+import getpass
+
 class Bank_Account:
-    def __init__(self,user_name,password,balance):
-        self.name=user_name
-        self.password=password
-        self.balance=balance
+    def __init__(self, user_name, password, balance):
+        self.name = user_name
+        self.password = password
+        self.balance = balance
     
-    def withdraw(self,amount):
-        self.balance-=amount
+    def withdraw(self, amount):
+        self.balance -= amount
 
-    def deposit(self,amount):
-        self.balance+=amount
+    def deposit(self, amount):
+        self.balance += amount
 
-object_1=Bank_Account('ABC','password',float('500000'))
-print(object_1.name,object_1.password,object_1.balance)
+# Function to create a list of Bank_Account objects
+def create_bank_accounts(n):
+    accounts = []
+    for i in range(n):
+        username = input("Enter username: ")
+        password = getpass.getpass("Enter password: ")
+        balance = float(input("Enter balance: "))
+        account = Bank_Account(username, password, balance)
+        accounts.append(account)
+    return accounts
 
+# Taking input from user for no. of accounts
+num_accounts = int(input("Enter the number of accounts to create: "))
 
-object_2=Bank_Account('XYZ','password',float('000000'))
-print("Before Withdraw")
-print(object_2.name,object_2.password,object_2.balance)
+# Creating bank accounts
+total_accounts = create_bank_accounts(num_accounts)
 
-print("After Withdraw")
-object_2.withdraw(400)
-print(object_2.name,object_2.password,object_2.balance)
+# Printing all created accounts
+print("\nAll accounts created:")
+for account in total_accounts:
+    print(account)
