68ec742c1f3e311053cfcd4fa0da32b94f82f667
jeenakarmi <jinanakarmi@gmail.com>
2024-03-23 16:44:32 +0545

    checking which  object it belongs to

--- banking.py
+++ banking.py
@@ -23,13 +23,34 @@ def create_bank_accounts(n):
         accounts.append(account)
     return accounts
 
+# Function to check username and password against created accounts
+def authenticate_user(username, password, accounts):
+    for account in accounts:
+        if account.name == username and account.password == password:
+            return account
+    return None
+
 # Taking input from user for no. of accounts
 num_accounts = int(input("Enter the number of accounts to create: "))
 
 # Creating bank accounts
 total_accounts = create_bank_accounts(num_accounts)
 
-# Printing all created accounts
-print("\nAll accounts created:")
+#for task 1
+#  Printing all created accounts
+# print("\nAll accounts created:")
+# for account in total_accounts:
+#     print(account)
+
+# Taking username and password from user for authentication
+username_input = input("Enter your username to authenticate: ")
+password_input = getpass.getpass("Enter your password: ")
+
+# Checking which object the provided username and password belong to
 for account in total_accounts:
-    print(account)
+    if account.name == username_input and account.password == password_input:
+        print("The provided username and password belong to the following account:")
+        print(account)
+        break
+else:
+    print("No account found with the provided username and password.")
